9ccf29d9609e7402fac11d7d1ba897315ec2cdff
kyle.canonigoa@cit.edu <kyle.canonigoa@cit.edu>
2023-10-03 22:09:16 +0800

    Fork Practice

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,7 +3,7 @@ package SimpleCalc;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+// Kyle A. Canonigo - Fork
 public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
     private JTextField tfNumber1;
