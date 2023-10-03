abf367fd0ab926bc092c922affb75d3b93d0174d
kyle.canonigoa@cit.edu <kyle.canonigoa@cit.edu>
2023-10-03 22:44:38 +0800

    Revert "dev branch"
    This reverts commit 3a832c1607f37d55e42d30d225890e31e493761c.

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 // Kyle A. Canonigo - Fork
-
-// Dev Branch
 public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
     private JTextField tfNumber1;
